7ece26e92426414b8d98704cc3d383dc15019006
Sean Galland <s_galland@msn.com>
2022-10-26 12:21:27 -0600

    Updated tests and added log display

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { ABILITIES, CLASSES, determineClass, rollDice } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test random number generator', () => {
+  for (let i = 0; i < 10000; i++) {
+    const value = rollDice();
+    expect(value).toBeGreaterThanOrEqual(1);
+    expect(value).toBeLessThanOrEqual(6);
+  }
+});
+
+test('test character type based on attributes', () => {
+  let abilityScores = {
+    [ABILITIES.STR]: rollDice(6, 3),
+    [ABILITIES.INT]: rollDice(6, 3),
+    [ABILITIES.WIS]: rollDice(6, 3),
+    [ABILITIES.DEX]: rollDice(6, 3),
+    [ABILITIES.CHA]: rollDice(6, 3),
+    [ABILITIES.CON]: rollDice(6, 3),
+  };
+  const charClass = determineClass(abilityScores);
+
+  const sortedAbilities = Object.values(abilityScores).map(score => { return { name: score[0], value: score[1] } }).sort((a, b) => b.value - a.value);
+
+  if (sortedAbilities[0].name === ABILITIES.STR) expect(charClass).toEqual(CLASSES.FIGHTER);
+  else if (sortedAbilities[0].name === ABILITIES.INT) expect(charClass).toEqual(CLASSES.MAGICUSER);
+  else if (sortedAbilities[0].name === ABILITIES.WIS) expect(charClass).toEqual(CLASSES.CLERIC);
+  else if (sortedAbilities[0].name === ABILITIES.INT) expect(charClass).toEqual(CLASSES.THIEF);
+
+  expect(charClass).toBeDefined();
 });
